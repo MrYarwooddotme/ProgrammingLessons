804cdc094a102e42dc295748f6947eabf74e7da2
Mr Yarwood <62912708+MrYarwooddotme@users.noreply.github.com>
2020-09-27 16:07:15 +0100

    Beatles

--- Lists/Lists/Module1.vb
+++ Lists/Lists/Module1.vb
@@ -1,17 +1,19 @@
 ﻿Module Module1
     Sub Main()
-        Dim names As New List(Of String)
-
-        names.Add("Matthew")
-
-        names.Add("Mark")
-
-        names.Add("Luke")
-
-        names.Add("John")
-
-        names.RemoveAt(1)
-
-        Console.WriteLine(names(1))
-    End Sub
+        dim myList As New List(Of String)
+        myList.Add("Apple")
+        myList.Add("Banana")
+        myList.Add("Cherry")
+        for i = 0 To myList.Count - 1
+            Console.WriteLine(myList(i))
+        Next
+        for i = 1 To 3
+            Console.WriteLine("Add a fruit")
+            myList.Add(console.ReadLine())
+        Next
+        for i = 0 To myList.Count - 1
+            Console.WriteLine(myList(i))
+        Next    
+        
+End Sub
 End Module
---
+++ beatlesList/beatlesList/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+    Sub Main()
+        dim beatles as new list(of String)
+        dim beatle As String
+        beatles.Add("Paul")
+        beatles.Add("John")
+        beatles.Add("Ringo")
+        beatles.Add("George")
+        beatles.Add("Pete")
+        beatles.Add("Stuart")
+        for i = 0 to beatles.count - 1
+            Console.WriteLine(beatles(i))
+        Next
+        Console.WriteLine("Who would you like to remove?")
+        beatle = console.ReadLine()
+        if Char.isDigit(beatle) Then
+            beatles.RemoveAt(beatle)
+        Else
+            for b = 0 to beatles.count - 1
+            beatles.Remove(beatle)    
+            Next
+        End If
+        
+        for c = 0 to beatles.count - 1
+            Console.WriteLine(beatles(c))
+        Next  
+    End Sub
+End Module
