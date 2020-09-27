6c2d55b0cb9773a7ea9b15d9faf3e9257616f69a
Mr Yarwood <62912708+MrYarwooddotme@users.noreply.github.com>
2020-09-27 14:30:20 +0100

    lists

--- Lists/Lists/Module1.vb
+++ Lists/Lists/Module1.vb
@@ -1,4 +1,17 @@
 ﻿Module Module1
     Sub Main()
+        Dim names As New List(Of String)
+
+        names.Add("Matthew")
+
+        names.Add("Mark")
+
+        names.Add("Luke")
+
+        names.Add("John")
+
+        names.RemoveAt(1)
+
+        Console.WriteLine(names(1))
     End Sub
 End Module
